e5cdab51beda53c1263c791354188bcde59381b4
Muhammad Nasiruddin <106563433+MuhammadNasir-44@users.noreply.github.com>
2024-03-05 20:55:36 +0000

    Update datacleaning_bookscsv.R

--- Project_2/datacleaning2_bookcsv/datacleaning_bookscsv.R
+++ Project_2/datacleaning2_bookcsv/datacleaning_bookscsv.R
@@ -55,8 +55,8 @@ book_words %>%
   anti_join(stop_words) %>% 
   count(word, sort = TRUE, name = "freq")
 ```
-"Now to generates word clouds for specific titles in the book_words dataset. 
-It first removes common words using the anti_join() function, filters the dataset for a specific title (e.g., "Wuthering Heights"), 
+#### Now to generates word clouds for specific titles in the book_words dataset. ####
+"It first removes common words using the anti_join() function, filters the dataset for a specific title (e.g., "Wuthering Heights"), 
 counts the frequency of each word, filters out words with frequencies less than 10, and finally creates a word cloud using the wordcloud2() function."
 
 ```
@@ -68,8 +68,29 @@ book_words %>%
   wordcloud2()
 book_words
 ```
+#### Now the next step is to performs sentiment analysis using the AFINN lexicon on the clean_afinn.csv dataset. ####
+"It reads the CSV file into the book_raw dataframe, filters out rows where the word is not "miss", and assigns the result to the sentiment variable. 
+The purpose of this code is to analyze sentiment scores associated with words in the dataset, providing insights into the sentiment expressed in the text data."
 
+```
+sentiment <- 
+  book_raw <- read_csv("clean_afinn.csv", show_col_types = FALSE) %>% 
+  #read_csv(here("Data", "clean_afinn.csv")) %>% 
+  filter(word != "miss") # Why have I removed this?
+sentiment
+```
+###Data Cleaning: Removing 'miss' Entries ####
+"The code snippet reads the clean_afinn.csv dataset into the book_raw dataframe and filters out rows where the word is "miss". 
+This step is performed as part of data cleaning to remove irrelevant or erroneous entries from the dataset.
+The rationale for removing these entries is provided with a comment in the code."
 
+```
+book_words %>% 
+  semi_join(sentiment %>% filter(value>0)) %>% 
+  filter(title == "Wuthering Heights") %>%
+  count(word, sort = TRUE, name = "freq") %>% 
+  wordcloud2()
+```
 
 
 
